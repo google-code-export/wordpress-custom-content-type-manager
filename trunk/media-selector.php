56d14755629cd3285b92662c69c37d4b6c77fd07
fireproofsocks <fireproofsocks@de83203f-eb1a-3a46-e551-23e103716cc5>
2010-10-11 23:54:55 +0000

    GOT IT. Image custom fields now working.

--- trunk/media-selector.php
+++ trunk/media-selector.php
@@ -30,17 +30,8 @@ stdClass Object
     [comment_count] => 0
     [filter] => raw
 )
-
-$postslist = get_posts('post_type=attachment&numberposts=10&order=ASC&orderby=title');
-foreach ($postslist as $post)
-{
-	$id = $post->ID;
-	// keyword (thumbnail, medium, large or full)
-	print wp_get_attachment_image( $id, 'thumbnail' ) . '<br/>';
-	print $post->post_title . "<br/>";
-}
-
 ------------------------------------------------------------------------------*/
+// To tie into WP, we come in through the backdoor, by including the config.
 require_once( realpath('../../../').'/wp-config.php' );
 
 if ( !current_user_can('upload_files') )
@@ -48,97 +39,111 @@ if ( !current_user_can('upload_files') )
 	wp_die(__('You do not have permission to upload files.'));
 }
 
-// Controlled by URL parameters
-$default_search_args = array(
+// Used by the get_posts() function, but some of these we need to manipulate via URL params.
+$args = array(
+	'post_type' => 'attachment',
+	'numberposts' => 10,
 	'post_mime_type' => 'image',
-	'page' => 0,
-	'm' => '', # month
-	's'	=> '', # Search term
-
+	'order' => 'ASC',
+	'orderby' => 'title',
 );
+$page = 0;
+$m = '';
+$fieldname = 'media_field';
+
+// Read supplied get args. Filter them!!!
+if ( isset($_GET['post_mime_type']) && !empty($_GET['post_mime_type']) )
+{
+	if ( preg_match('/[^a-z]/', $_GET['post_mime_type']) )
+	{
+		exit;  // Only a-z is allowed.
+	}
+	$args['post_mime_type'] = $_GET['post_mime_type'];
+}
+if ( isset($_GET['fieldname']) && !empty($_GET['fieldname']) )
+{
+	if ( preg_match('/[^a-z_\-]/i', $_GET['post_mime_type']) )
+	{
+		exit;  // Only a-z, _, - is allowed.
+	}
+	$fieldname = $_GET['fieldname'];
+}
+
+if ( isset($_GET['page']))
+{
+	$page = (int) $_GET['page'];
+}
+if ( isset($_GET['m']))
+{
+	$page = (int) $_GET['m'];
+}
+
 
 //$post_mime_type = $_GET['post_mime_type'];
 //------------------------------------------------------------------------------
 ?>
 <style>
-	.mini-thumbnail {
-		vertical-align:middle;
-	}
-	.media_detail {
-		display:none;
-		margin-left:40px;
-	}
-	.toggler {
-		float: right;
-		margin-right: 200px;
-	}
+	<?php print file_get_contents('css/media_selector.css'); ?>
 </style>
 <script type="text/javascript" src="../../../../wp-includes/js/jquery/jquery.js"></script>
 <script type="text/javascript">
-	jQuery(document).ready(main);
-
-	/* Listeners */
-	function main()
-	{
-	}
-
 	function toggle_image_detail(css_id)
 	{
-		console.log(css_id);
 		jQuery('#'+css_id).slideToggle(400);
     	return false;
 	}
-
+	
+	function update_selection(id, thumbnail_html)
+	{
+		jQuery('#<?php print $fieldname; ?>').val(id);
+		jQuery('#<?php print $fieldname; ?>_media').html(thumbnail_html);		
+	}
 </script>
 
 
 <div id="media-items">
 
 <?php
-$args = array(
-	'post_type' => 'attachment',
-	'numberposts' => 3,
-	'post_mime_type' => 'image',
-	'order' => 'ASC',
-	'orderby' => 'title',
-	'field_name' => '',	# gotta use this in the radio buttons
-
-);
-// Read supplied get args. Filter them!!!
-foreach ($args as $k => $v)
-{
-	if ( isset($_GET[$k]))
-	{
-		$args[$k] = $_GET[$k];
-	}
-}
-
+//------------------------------------------------------------------------------
 $postslist = get_posts($args);
-//$postslist = get_posts('post_type=attachment&mime_type=image&numberposts=10&order=ASC&orderby=title');
+//print_r($postslist); exit;
 foreach ($postslist as $post):
-
+//------------------------------------------------------------------------------
 	$id = $post->ID;
-	// keyword (thumbnail, medium, large or full)
-	//print $id; exit;
-//	$src = wp_get_attachment_image( $id, 'thumbnail' );
-	list($src, $thumb_w, $thumb_h) = wp_get_attachment_image_src( $id, 'thumbnail' );
-	list($med_src, $med_w, $med_h) = wp_get_attachment_image_src( $id, 'medium' );
-	list($full_src, $w, $h) = wp_get_attachment_image_src( $id, 'full' );
+	$thumbnail_html = '';
+	$medium_html = '';
+	// It's an image
+	if (preg_match('/^image/', $post->post_mime_type) )
+	{
+		list($src, $w, $h) = wp_get_attachment_image_src( $post->ID, 'thumbnail');
+		$thumbnail_html = sprintf('<img class="mini-thumbnail" src="%s" height="30" width="30" alt="" />'
+			, $src);
+		$medium_html = wp_get_attachment_image( $post->ID, 'medium' );
+		$preview_html = wp_get_attachment_image( $post->ID, 'thumbnail' );
+	}
+	// It's not an image
+	else
+	{
+		list($src, $w, $h) = wp_get_attachment_image_src( $post->ID, 'thumbnail', TRUE );
+		$thumbnail_html = sprintf('<img src="%s" class="attachment-medium" width="30" height="30" alt=""/>', $src);
+		$medium_html = wp_get_attachment_image( $post->ID, 'medium', TRUE );
+		$preview_html = wp_get_attachment_image( $post->ID, 'thumbnail', TRUE );
+	}
+	# Passed via JS, so we gotta prep it.
+	$preview_html = preg_replace('/"/', "'", $preview_html); 
+	$preview_html = preg_replace("/'/", "\'", $preview_html);
 	
-	preg_match('#.*/(.*)$#', $full_src, $matches);
+	preg_match('#.*/(.*)$#', $post->guid, $matches);
 	$filename = $matches[1];
-//	$title = $post->post_title . "<br/>";
 ?>
-
 <div id="media-item-<?php print $post->ID; ?>">
-
-	
 	<div width="400px">
-		<input type="radio" name="" id="media-option-<?php print $post->ID; ?>"> 
+		<input type="radio" name="<?php print $fieldname;?>" id="media-option-<?php print $post->ID; ?>"> 
 		<label for="media-option-<?php print $post->ID; ?>">
-			<img class="mini-thumbnail" src="<?php print $src; ?>" height="30" width="30" alt='' />
+			<?php print $thumbnail_html; ?>			
 			<span class="title"><?php print $post->post_title; ?></span>
 		</label>
+		<span class="select_me" onclick="javascript:update_selection('<?php print $post->ID; ?>','<?php print $preview_html; ?>')">Select me</span>
 		<span class="toggler" onclick="javascript:toggle_image_detail('media-detail-<?php print $post->ID; ?>');">Show</span>
 	</div>
 	
@@ -148,14 +153,15 @@ foreach ($postslist as $post):
 				<tr valign='top'>
 					<td class="A1B1" id="thumbnail-head-<?php print $post->ID; ?>">
 						<p>
-						<img class="thumbnail" src="<?php print $med_src; ?>" height="<?php print $med_h;?>" width="<?php print $med_w; ?>" alt=''/></p>
+							<?php print $medium_html; ?>
+						</p>
 					</td>
 					<td>
 						<p><strong>File name:</strong> <?php print $filename; ?></p>
 						<p><strong>File type:</strong> <?php print $post->post_mime_type; ?></p>	
 						<p><strong>Upload date:</strong> <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?></p>
 						<p><strong>Dimensions:</strong> <span id='media-dims-<?php print $post->ID; ?>'><?php print $h; ?>&nbsp;&times;&nbsp;<?php print $w; ?></span></p>
-						<p><a href='http://localhost:8888/?attachment_id=<?php print $post->ID; ?>' target='_blank'>View</a></p>
+						<p><a href='http://localhost:8888/?attachment_id=<?php print $post->ID; ?>' target='_blank'>View Original</a></p>
 					</td>
 				</tr>
 			</thead>
